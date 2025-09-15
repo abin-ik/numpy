b7f60f553e11deb9e09d3201f0e91dd95d090bc6
Abin Thomas <abin29192@gmail.com>
2025-09-15 20:48:34 +0530

    Add files via upload

---
+++ sep15d.py
@@ -0,0 +1,15 @@
+class calculator():
+
+    def add(self,a,b):
+        return a+b
+    def sub(self,a,b):
+        return a-b
+    def mul(self,a,b):
+        return a*b
+    def div(self,a,b):
+        return a/b
+
+class display():
+
+    def display1(self):
+        print("hi")
